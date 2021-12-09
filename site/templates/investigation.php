2ca31a217e22aa40feb405587bfc35d68d944723
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2021-12-09 09:15:18 +0100

    added styling for project

--- site/templates/investigation.php
+++ site/templates/investigation.php
@@ -4,32 +4,56 @@
 
 
 
-<div class="container">           
+<div class="container">
+    <div class="breadcrumb" itemprop="breadcrumb">
+        <?php snippet('general/breadcrumb-content') ?>
+    </div>
 
-    <?php //ALL BLOGS DESKTOP ?>
-    <div class="blog-wrapper-desktop">
-        <?php  
+
+
+    <div class="blog-wrapper">
+    <?php
         $projects = $page->children()->listed();
-        foreach ($projects as $project): ?>
+        foreach ($projects as $project) : ?>
             <project class="blog-wrapper__blog-overview">
                 <div>
-                    <img class="blog-wrapper__blog-overview__img img" src="<?=$project->image()->url()?>" alt="<?=$project->image()->alt()?>">
+                    <img class="blog-wrapper__blog-overview__img img" src="<?= $project->image()->url() ?>" alt="<?= $project->image()->alt() ?>">
 
                     <div class="blog-wrapper__blog-overview__text-wrapper">
-                        <h2 class="blog-wrapper__blog-overview__text-wrapper__title h2"><?=$project->title()->html()?></h2>
-                        <p class="blog-wrapper__blog-overview__text-wrapper__p p"><?=$project->paragraph()->excerpt(90)?></p>
+                        <h2 class="blog-wrapper__blog-overview__text-wrapper__title h2"><?= $project->title()->html() ?></h2>
+                        <p class="blog-wrapper__blog-overview__text-wrapper__p p"><?= $project->paragraph()->excerpt(90) ?></p>
+
+                        <a class="blog-wrapper__blog-overview__text-wrapper__link button-small" href="<?= $project->url() ?>">Lees onderzoek</a>
+                    </div>
+                </div>
+            </project>
+        <?php endforeach ?>
+    </div>
+
+
+
+    <div class="blog-wrapper-desktop">
+        <?php
+        $projects = $page->children()->listed();
+        foreach ($projects as $project) : ?>
+            <project class="blog-wrapper__blog-overview">
+                <div>
+                    <img class="blog-wrapper__blog-overview__img img" src="<?= $project->image()->url() ?>" alt="<?= $project->image()->alt() ?>">
+
+                    <div class="blog-wrapper__blog-overview__text-wrapper">
+                        <h2 class="blog-wrapper__blog-overview__text-wrapper__title h2"><?= $project->title()->html() ?></h2>
+                        <p class="blog-wrapper__blog-overview__text-wrapper__p p"><?= $project->paragraph()->excerpt(90) ?></p>
                     </div>
                 </div>
 
                 <div>
-                    <a class="blog-wrapper__blog-overview__text-wrapper__link button-small" href="<?=$project->url()?>">Lees artikel</a>
+                    <a class="blog-wrapper__blog-overview__text-wrapper__link button-small" href="<?= $project->url() ?>">Lees onderzoek</a>
                 </div>
             </project>
-        <?php endforeach?>
+        <?php endforeach ?>
     </div>
-    
 </div>
 
 
 
-<?php snippet('general/footer')?>
+<?php snippet('general/footer') ?>
--- site/templates/project.php
+++ site/templates/project.php
@@ -3,12 +3,52 @@
 <?php snippet('menu/menu-white') ?>
 
 <div class="container">
-    <h1><?= $page->title() ?></h1>
-    <p><?= $page->paragraph() ?></p>
+    <div class="breadcrumb" itemprop="breadcrumb">
+        <?php snippet('general/breadcrumb-content') ?>
+    </div>
 
-    <img src="<?= $page->image()->url()?>"/>
 
+
+    <section class="content-article content-project">
+        <h1 class="content-article__title h1"><?= $page->title() ?></h1>
+
+
+
+        <?php if($pdf = $page->pdf()->toFile()): ?>
+            <a class="content-article__document" href="<?= $pdf->url() ?>"><i class="fa fa-file-text" aria-hidden="true" download></i> Download PDF</a>
+        <?php endif; ?>
+
+        <?php if($scription = $page->scription()->toFile()): ?>
+            <a class="content-article__document" href="<?= $scription->url() ?>"><i class="fa fa-file-text" aria-hidden="true" download></i> Download Scriptie</a>
+        <?php endif; ?>
+
+
+
+        <div class="flex-row-desktop">
+            <p class="content-article__text-wrapper__p p"><?= $page->firstParagraphProject() ?></p>
+
+            <div>
+                <?php if($firstImage = $page->firstImageProject()->toFile()): ?>
+                    <img class="content-article__image img" src="<?= $firstImage->url() ?>" alt="<?= $firstImage->alt() ?>">
+                    <p class="p"><?= $firstImage->alt() ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+
+
+
+        <div class="flex-row-desktop-reverse">
+            <p class="content-article__text-wrapper__p p"><?= $page->secondParagraphProject() ?></p>
+
+            <div>
+                <?php if($secondImage = $page->secondImageProject()->toFile()): ?>
+                    <img class="content-article__image img" src="<?= $secondImage->url() ?>" alt="<?= $secondImage->alt() ?>">
+                    <p class="p"><?= $secondImage->alt() ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
 </div>
 
 
-<?php snippet('general/footer')?>
+<?php snippet('general/footer') ?>
